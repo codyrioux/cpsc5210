6d64e81329892f24c798a5761de6f369ff0494c2
Cody Rioux <cody.rioux@gmail.com>
2013-04-16 14:41:49 -0600

    Updated the project file to contain relevant information.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cpsc5210 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A genetic algorithm for synthesizing reversible logic circuits."
+  :url "https://www.github.com.com/codyrioux/cpsc5210"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
